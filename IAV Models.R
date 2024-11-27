16c4856804934f7c260ee46130c10b83656616e4
Jordan Bone <j.bone.1@research.gla.ac.uk>
2024-11-27 15:48:28 +0000

    f

--- IAV Models.R
+++ IAV Models.R
@@ -1,7 +1,7 @@
 knitr::opts_chunk$set(echo=FALSE,warning=FALSE,message=FALSE,root.dir= "/Users/jordanbone/Documents/IAV.AI")
-source("functions.R") 
+source("functions.R")
 # source("MegaLibrary.R")
-taxize_options(ncbi_sleep = 1)
+# taxize_options(ncbi_sleep = 1)
 reference_table <- read.csv("Reference Table 2.csv")
 
 SEG <- c("01PB2","02PB1","03PA","04HA","05NP","06NA","07MP","08NS")
@@ -38,3 +38,15 @@ haMa <- data.frame("Base"=1:566,"Entropy"=Bios2cor::entropy(haMafas),row.names =
 ggplot(haBi,aes(Base,Entropy))+geom_line(colour="indianred",alpha=0.8)+geom_line(data=haMa,colour="slateblue",alpha=0.8)+
   geom_label(aes(350,0.955,label="Avian\nH = 93.13"),colour="indianred")+
   geom_label(aes(300,0.85,label="Mammalian\nH = 90.30"),colour="slateblue")+labs(title="Sitewise Diversity in Avian and Mammalian Haemagglutinin Clusters")
+
+### Lolliplots
+library(trackViewer)
+HASNPcon <- c(144,467)
+sample.HAcon <- GRanges("HA", IRanges(HASNPcon, width=1, names=c("Gly 144 Asp","Arg 467 Ser")))
+features.HA <- GRanges("HA", IRanges(c(1,1,17,52,276,330,347,514),width=c(565,16,51,223,53,16,166,20),names=c("HA","Signal Peptide","HA1 - Stalk","HA1 - Head","HA1 - Stalk","Fusion Peptide","HA2 - Stalk","Transmembrane Domain")))
+features.HA$fill <- c("ivory","tan","plum3","purple3","plum3","firebrick2","lightslateblue","sienna1")
+features.HA$height <- 0.08
+sample.HAcon$color <- rep("dodgerblue2",2)
+sample.HAcon$shape <- rep("circle",2)
+trackViewer::lolliplot(sample.HAcon,features.HA,legend = legend)
+grid.text("HA Segment", x=.5, y=.98, just="top",gp=gpar(cex=1.5, fontface="bold"))
