539aff6262f71a3155e725b7c5f3135ec6015375
Jordan Bone <j.bone.1@research.gla.ac.uk>
2024-11-28 14:31:20 +0000

    ft

--- IAV Models.R
+++ IAV Models.R
@@ -2,7 +2,15 @@
 # source("MegaLibrary.R")
 library("ggplot2")
 library("dplyr")
-reference_table <- read.csv("Reference Table 2.csv")
+library("tidyr")
+library("stringr")
+library("ape")
+library("trackViewer")
+library("Bios2cor")
+library("readxl")
+library("lubridate")
+reference_table <- read.csv("RefTable.csv")
+# write.csv(reference_table,"RefTable.csv",row.names = F)
 
 SEG <- c("01PB2","02PB1","03PA","04HA","05NP","06NA","07MP","08NS")
 PRT <- c("01PB2","02PB1","03PA","04HA","05NP","06NA","07M1","08NS1","07M2","08NEP")
@@ -18,29 +26,35 @@ ggplot(subset(mam,N>19),aes(Family,log10(N),fill=Subtype))+geom_col(position="do
 # ggsave("Mammal Subtypes.pdf",width=13,height=8)
 
 ######
+ssnl <- read_xlsx("Human Seasonals.xlsx",sheet="Sheet5")
+ggplot(ssnl,aes(Release_Date,WGS,colour=Genotype))+geom_point(alpha=0.7)
 
+######
 mod_cls <- read.csv("Sequences/Clusters.csv")
 ggplot(mod_cls,aes(Protein,Clusters,fill=Group))+geom_col(position="dodge")+
   facet_wrap(~Identity,scales = "free")+
   theme(legend.position = "bottom",axis.text.x = element_text(angle = 90))
 
-mod_cls %>% pivot_wider(names_from = c("Group","Identity"),values_from = "Clusters") %>% kbl(col.names = c("Protein","100%","95%","85%","75%","100%","95%","85%","75%"),align='c',caption="Number of representative sequences from each group") %>% kable_styling(full_width = F) %>% add_header_above(c(" "=1,"Avian"=4,"Mammalian"=4))
+mod_cls %>% pivot_wider(names_from = c("Group","Identity"),values_from = "Clusters") 
+# %>% kbl(col.names = c("Protein","100%","95%","85%","75%","100%","95%","85%","75%"),align='c',caption="Number of representative sequences from each group") %>% kable_styling(full_width = F) %>% add_header_above(c(" "=1,"Avian"=4,"Mammalian"=4))
 
+# a minimum coverage (option -c [0,1], which is defined by the number of aligned residue pairs divided by either the maximum of the length of query/centre and target/non-centre sequences alnRes/max(qLen,tLen) (default mode, --cov-mode 0), or by the length of the target/non-centre sequence alnRes/tLen (--cov-mode 1), or by the length of the query/centre alnRes/qLen (--cov-mode 2).
+
+# a minimum sequence identity (--min-seq-id [0,1]) with option --alignment-mode 3 defined as the number of identical aligned residues divided by the number of aligned columns including internal gap columns, or, by default, defined by a highly correlated measure, the equivalent similarity score of the local alignment (including gap penalties) divided by the maximum of the lengths of the two locally aligned sequence segments. The score per residue equivalent to a certain sequence identity is obtained by a linear regression using thousands of local alignments as training set.
 ######
 
 ### Sequence Diversity
 
-haBifas <- read.fasta("Sequences/Avian/ha_cluster_95_rep_seq.fasta")
-haMafas <- read.fasta("Sequences/Mammals/ha_cluster_rep_95.fasta")
+haBifas <- seqinr::read.fasta("Sequences/Avian/04HA/HA_cluster_95.fasta")
 haBi <- data.frame("Base"=1:567,"Entropy"=Bios2cor::entropy(haBifas),row.names = NULL)
+haMafas <- seqinr::read.fasta("Sequences/Mammals/04HA/HA_cluster_95.fasta")
 haMa <- data.frame("Base"=1:566,"Entropy"=Bios2cor::entropy(haMafas),row.names = NULL)
 
 ggplot(haBi,aes(Base,Entropy))+geom_line(colour="indianred",alpha=0.8)+geom_line(data=haMa,colour="slateblue",alpha=0.8)+
-  geom_label(aes(350,0.955,label="Avian\nH = 93.13"),colour="indianred")+
-  geom_label(aes(300,0.85,label="Mammalian\nH = 90.30"),colour="slateblue")+labs(title="Sitewise Diversity in Avian and Mammalian Haemagglutinin Clusters")
+  geom_label(aes(300,0.58,label="Avian\nH = 93.13"),colour="indianred")+
+  geom_label(aes(400,0.4,label="Mammalian\nH = 90.30"),colour="slateblue")+labs(title="Sitewise Diversity in Avian and Mammalian Haemagglutinin Clusters")
 
 ### Lolliplots
-library(trackViewer)
 HASNPcon <- c(144,467)
 sample.HAcon <- GRanges("HA", IRanges(HASNPcon, width=1, names=c("Gly 144 Asp","Arg 467 Ser")))
 features.HA <- GRanges("HA", IRanges(c(1,1,17,52,276,330,347,514),width=c(565,16,51,223,53,16,166,20),names=c("HA","Signal Peptide","HA1 - Stalk","HA1 - Head","HA1 - Stalk","Fusion Peptide","HA2 - Stalk","Transmembrane Domain")))
