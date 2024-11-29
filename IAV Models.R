4e5e7f2527d058c2c28012f28ea20eb0cebfd796
Jordan Bone <j.bone.1@research.gla.ac.uk>
2024-11-29 10:53:52 +0000

    ft

--- IAV Models.R
+++ IAV Models.R
@@ -18,8 +18,8 @@ ggplot(subset(mam,N>19),aes(Family,log10(N),fill=Subtype))+geom_col(position="do
 # ggsave("Mammal Subtypes.pdf",width=13,height=8)
 
 ######
-ssnl <- read_xlsx("Human Seasonals.xlsx",sheet="Sheet5")
-ggplot(ssnl,aes(Release_Date,WGS,colour=Genotype))+geom_point(alpha=0.7)
+# ssnl <- read_xlsx("Human Seasonals.xlsx",sheet="Sheet5")
+# ggplot(ssnl,aes(Release_Date,WGS,colour=Genotype))+geom_point(alpha=0.7)
 
 ######
 mod_cls <- read.csv("Sequences/Clusters.csv")
@@ -61,72 +61,81 @@ grid.text("HA Segment", x=.5, y=.98, just="top",gp=gpar(cex=1.5, fontface="bold"
 
 fea <- list.files("feats",pattern = ".csv",full.names = T,recursive = T)
 # fea <- fea[c(25,31)]
-
-for(i in 1:length(fea)){
-  print(fea[i])
-  tmp_ft <- read.csv(fea[i])
-  tmp_ft$ptAcc <- tmp_ft$X %>% str_split_i(":",1)
-  tmp_ft <- right_join(reference_table,tmp_ft)
-  fold_indices <- createMultiFolds(tmp_ft$UID, k = 5, times = 1)
-  preds <- tmp_ft[,c(-1:-16)] %>% remove_constant %>% names
-  # tmp_ft[,c(-2:-16)] %>% remove_constant %>% pivot_longer(cols = -1, names_to = "Ft",values_to = "Val") %>% subset(Val==0) %>% print()
-  
-  mo1 <- suppressWarnings(train(x = tmp_ft %>% select(all_of(preds)),y = tmp_ft %>% pull(Class),
-                         method="ranger",metric="ROC",
-                         preProc=c("center","scale"),num.trees=1000,importance="impurity",
-                         # weights=ifelse(tmp_ft$Class =="Aves",1,
-                         # (table(tmp_ft$Class)[1]/table(tmp_ft$Class)[2])),
-                         trControl = trainControl(method = "repeatedcv",number = 5,
-                                                  repeats = 1,index = fold_indices,
-                                                  classProbs = TRUE,savePredictions = TRUE,
-                         summaryFunction=twoClassSummary),
-                         tuneGrid = expand.grid(.splitrule = "gini",
-                                                .min.node.size = seq(from = 5, to = 45, length = 3),
-                                                .mtry = round(sqrt(length(preds))))))
-  
-  mo_nm <- fea[i] %>% str_split_i("\\/",2) %>% str_split("\\_")
-  saveRDS(mo1, file=paste0("Models/",mo_nm[[1]][1],"_",mo_nm[[1]][3],"_",
-                           mo_nm[[1]][4],"_",mo_nm[[1]][5],"_",str_replace_all(mo_nm[[1]][8], ".csv",""),".rds"))
-}
-
-### 
-
-# for(i in 1:10){
-#   print(PRT[i])
-#   tmer_mod <- readRDS(paste0("Models/wgs",PRT[i],"/",substr(PRT[i],3,8),"_wgs_2mer.rds"))
-#   ctdc_mod <- readRDS(paste0("Models/wgs",PRT[i],"/",substr(PRT[i],3,8),"_wgs_ctdc.rds"))
-#   ctdd_mod <- readRDS(paste0("Models/wgs",PRT[i],"/",substr(PRT[i],3,8),"_wgs_ctdd.rds"))
-#   ctdt_mod <- readRDS(paste0("Models/wgs",PRT[i],"/",substr(PRT[i],3,8),"_wgs_ctdt.rds"))
-#   ctri_mod <- readRDS(paste0("Models/wgs",PRT[i],"/",substr(PRT[i],3,8),"_wgs_ctriad.rds"))
-#   psea_mod <- readRDS(paste0("Models/wgs",PRT[i],"/",substr(PRT[i],3,8),"_wgs_pseaac.rds"))
+# 
+# for(i in 1:length(fea)){
+#   print(fea[i])
+#   tmp_ft <- read.csv(fea[i])
+#   tmp_ft$ptAcc <- tmp_ft$X %>% str_split_i(":",1)
+#   tmp_ft <- right_join(reference_table,tmp_ft)
+#   fold_indices <- createMultiFolds(tmp_ft$UID, k = 5, times = 1)
+#   preds <- tmp_ft[,c(-1:-16)] %>% remove_constant %>% names
+#   # tmp_ft[,c(-2:-16)] %>% remove_constant %>% pivot_longer(cols = -1, names_to = "Ft",values_to = "Val") %>% subset(Val==0) %>% print()
 #   
-#   # caretEnsemble(c("two_mer"=tmer_mod,"CTDc"=ctdc_mod,"CTDd"=ctdd_mod,"CTDt"=ctdt_mod,"triad"=ctri_mod,"pseaac"=psea_mod),preProc = c("center", "scale"),metric = "ROC") %>% saveRDS(paste0("Models/Complete/",substr(PRT[i],3,8),"_roc.rds"))
+#   mo1 <- suppressWarnings(train(x = tmp_ft %>% select(all_of(preds)),y = tmp_ft %>% pull(Class),
+#                          method="ranger",metric="ROC",
+#                          preProc=c("center","scale"),num.trees=1000,importance="impurity",
+#                          # weights=ifelse(tmp_ft$Class =="Aves",1,
+#                          # (table(tmp_ft$Class)[1]/table(tmp_ft$Class)[2])),
+#                          trControl = trainControl(method = "repeatedcv",number = 5,
+#                                                   repeats = 1,index = fold_indices,
+#                                                   classProbs = TRUE,savePredictions = TRUE,
+#                          summaryFunction=twoClassSummary),
+#                          tuneGrid = expand.grid(.splitrule = "gini",
+#                                                 .min.node.size = seq(from = 5, to = 45, length = 3),
+#                                                 .mtry = round(sqrt(length(preds))))))
 #   
-#   caretEnsemble(c("two_mer"=tmer_mod,"CTDc"=ctdc_mod,"CTDd"=ctdd_mod,"CTDt"=ctdt_mod,"triad"=ctri_mod,"pseaac"=psea_mod),
-#                 # preProc = c("center", "scale"),
-#                 metric = "kappa") 
-#   # %>% saveRDS(paste0("Models/Complete/",substr(PRT[i],3,8),"_wgs_K.rds"))
+#   mo_nm <- fea[i] %>% str_split_i("\\/",2) %>% str_split("\\_")
+#   saveRDS(mo1, file=paste0("Models/",mo_nm[[1]][1],"_",mo_nm[[1]][3],"_",
+#                            mo_nm[[1]][4],"_",mo_nm[[1]][5],"_",str_replace_all(mo_nm[[1]][8], ".csv",""),".rds"))
 # }
+# 
+# ### 
 
 mods <- list.files("Models",pattern = ".rds",full.names = T,recursive = F)
 mods <- unique(substr(mods,1,21))
-for(i in 1:length(mods)){
-  print(mods[i])
-  tmer_mod <- readRDS(paste0(mods[i],"_2mer.rds"))
-  ctdc_mod <- readRDS(paste0(mods[i],"_ctdc.rds"))
-  ctdd_mod <- readRDS(paste0(mods[i],"_ctdd.rds"))
-  ctdt_mod <- readRDS(paste0(mods[i],"_ctdt.rds"))
-  ctri_mod <- readRDS(paste0(mods[i],"_ctriad.rds"))
-  psea_mod <- readRDS(paste0(mods[i],"_pseaac.rds"))
-
-  # caretEnsemble(c("two_mer"=tmer_mod,"CTDc"=ctdc_mod,"CTDd"=ctdd_mod,"CTDt"=ctdt_mod,"triad"=ctri_mod,"pseaac"=psea_mod),preProc = c("center", "scale"),metric = "ROC") %>% saveRDS(paste0("Models/Complete/",substr(PRT[i],3,8),"_roc.rds"))
-
-  caretEnsemble(c("two_mer"=tmer_mod,"CTDc"=ctdc_mod,"CTDd"=ctdd_mod,"CTDt"=ctdt_mod,"triad"=ctri_mod,"pseaac"=psea_mod),preProc = c("scale"),metric = "ROC") %>% saveRDS(paste0("Comp/Ensemble/",mods[i],".rds"))
-  caretStack(c("two_mer"=tmer_mod,"CTDc"=ctdc_mod,"CTDd"=ctdd_mod,"CTDt"=ctdt_mod,"triad"=ctri_mod,"pseaac"=psea_mod),preProc = c("scale"),metric = "ROC") %>% saveRDS(paste0("Comp/Stack/",mods[i],".rds"))
+# for(i in 1:length(mods)){
+#   print(mods[i])
+#   tmer_mod <- readRDS(paste0(mods[i],"_2mer.rds"))
+#   ctdc_mod <- readRDS(paste0(mods[i],"_ctdc.rds"))
+#   ctdd_mod <- readRDS(paste0(mods[i],"_ctdd.rds"))
+#   ctdt_mod <- readRDS(paste0(mods[i],"_ctdt.rds"))
+#   ctri_mod <- readRDS(paste0(mods[i],"_ctriad.rds"))
+#   psea_mod <- readRDS(paste0(mods[i],"_pseaac.rds"))
+# 
+#   caretEnsemble(c("two_mer"=tmer_mod,"CTDc"=ctdc_mod,"CTDd"=ctdd_mod,"CTDt"=ctdt_mod,"triad"=ctri_mod,"pseaac"=psea_mod),preProc = c("scale"),metric = "ROC") %>% saveRDS(paste0("Comp/Ensemble/",mods[i],".rds"))
+#   caretStack(c("two_mer"=tmer_mod,"CTDc"=ctdc_mod,"CTDd"=ctdd_mod,"CTDt"=ctdt_mod,"triad"=ctri_mod,"pseaac"=psea_mod),preProc = c("scale"),metric = "ROC") %>% saveRDS(paste0("Comp/Stack/",mods[i],".rds"))
+# }
+# 
+modc <- list.files("Comp",pattern = ".rds",full.names = T,recursive = T)
+for(i in 1:length(modc)){
+  ModMeth <- str_split_i(modc[i],"\\/",2)
+  ModParam <- str_split_i(modc[i],"\\/",4) %>% str_replace_all(".rds","")
+  MoDo <- paste0(ModMeth,"_",ModParam)
+  assign(MoDo, readRDS(modc[i]))
 }
 
-HA_t1 <- readRDS("Comp/Ensemble/Models/ha_95_c50_cov1.rds")
-HA_t2 <- readRDS("Comp/Stack/Models/ha_95_c50_cov1.rds")
+# Stack_ha_95_c50_cov1
+summary(Stack_ha_95_c50_cov1)
+# Stack_ha_95_c50_cov0
+summary(Stack_ha_95_c50_cov0)
+# Stack_ha_95_c70_cov1
+summary(Stack_ha_95_c70_cov1)
+# Stack_ha_95_c70_cov0
+summary(Stack_ha_95_c70_cov0)
+# Stack_ha_95_c80_cov1
+summary(Stack_ha_95_c80_cov1)
+# Stack_ha_95_c80_cov0
+summary(Stack_ha_95_c80_cov0)
 
-summary(HA_t1)
-summary(HA_t2)
+# Ensemble_ha_95_c50_cov1
+summary(Ensemble_ha_95_c50_cov1)
+# Ensemble_ha_95_c50_cov0
+summary(Ensemble_ha_95_c50_cov0)
+# Ensemble_ha_95_c70_cov1
+summary(Ensemble_ha_95_c70_cov1)
+# Ensemble_ha_95_c70_cov0
+summary(Ensemble_ha_95_c70_cov0)
+# Ensemble_ha_95_c80_cov1
+summary(Ensemble_ha_95_c80_cov1)
+# Ensemble_ha_95_c80_cov0
+summary(Ensemble_ha_95_c80_cov0)
