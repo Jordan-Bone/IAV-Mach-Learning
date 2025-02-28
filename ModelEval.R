0c68a3744720e43c59edb0e5cd785ef1cc6adcb2
Jordan Bone <j.bone.1@research.gla.ac.uk>
2025-02-28 10:42:12 +0000

    Confusion Matrices

--- ModelEval.R
+++ ModelEval.R
@@ -5,8 +5,12 @@ uid_ref <- read.csv("USED UIDS.csv")
 ft_data <- read.csv("Comp/Validata.csv") %>% left_join(uid_ref,by="UID",keep = F)
 # ft_data %>% filter(!complete.cases(.)) %>% write.csv("Bad cases.csv")
 # for(i in 1:length(ft_data)){if(!is.numeric(ft_data[,i])){print(i)}}
+master2 <- readRDS("Comp/ConfMatrix 2class Master.rds")
+masterM <- readRDS("Comp/ConfMatrix Multiclass Master.rds")
+prds <- c()
+vlis <- c()
 
-mods <- list.files("Comp",pattern="2class.rds",full.names = T)
+mods <- list.files("Comp",pattern="class.rds",full.names = T)
 for(j in c(1:length(mods))){
   print(j)
   print(mods[j])
@@ -23,35 +27,35 @@ for(j in c(1:length(mods))){
   predict_class <- predict(MOD, newdata=validate)
   predict_class$Prediction <- apply(predict_class, 1, function(x) colnames(predict_class)[which.max(x)])
   
-  prds <- append(prds,predict_class$Prediction)
-  vlis <- append(vlis,validate$Classification)
+  try({
+    cm <- confusionMatrix(predict_class$Prediction %>% as.factor %>% droplevels,
+                          validate$Classification %>% as.factor %>% droplevels)
+    saveRDS(cm,file=paste0(mods[j] %>% str_replace(".rds","_CoMa"),".rds"))
+  },silent = T)
+  
+  predict_class <- c()
+  validate <- c()
+  # prds <- append(prds,predict_class$Prediction)
+  # vlis <- append(vlis,validate$Classification)
 }
 
-# cm <- confusionMatrix(predict_class$Prediction %>% droplevels,
-#                           validate$Classification %>% as.factor %>% droplevels)
 # confusionMatrix(prds %>% as.factor, vlis %>% as.factor) %>% saveRDS("ConfMatrix Master 2class.rds")
-# saveRDS(cm,file=paste0(mods[j] %>% str_replace(".rds","_CoMa"),".rds"))
-    
-    if(length(levels(MOD$models$CTDc.ranger$trainingData$.outcome))>2){
-      matrix_one_vs_all <- vector("list", length(levels(MOD$models$CTDc.ranger$trainingData$.outcome)))
-      for (i in seq_along(matrix_one_vs_all)) {
-        positive.class <- levels(MOD$models$CTDc.ranger$trainingData$.outcome)[i]
-        # print(positive.class)
-        matrix_one_vs_all[[i]] <- confusionMatrix(predict_class$Prediction %>% droplevels, 
-                                                  validate %>% pull(Classification) %>% as.factor %>% droplevels, 
-                                                  positive = positive.class)
-        # t1_df <- cm$overall %>% round(., 3) %>% t() %>%
-        #   cbind(., 
-        #         AUC = multiclass.roc(response = validate %>% pull(Classification), predictor = predict_class) %>% 
-        #           .$auc %>% as.numeric() %>% round(3),
-        #         micro_F1 = matrix_one_vs_all %>% get.micro.f1() %>% round(3),
-        #         macro_F1 = matrix_one_vs_all %>% get.macro.f1() %>% round(3))  
-  # if (output == "classwise"){return(matrix_test$byClass %>% reshape2::melt())}
-  # else {return(as.data.frame(t1_df))}
-}}
+
+matrix_one_vs_all <- vector("list", length(levels(MOD$models$CTDc.ranger$trainingData$.outcome)))
+for (i in seq_along(matrix_one_vs_all)) {
+positive.class <- levels(MOD$models$CTDc.ranger$trainingData$.outcome)[i]
+# print(positive.class)
+matrix_one_vs_all[[i]] <- confusionMatrix(predict_class$Prediction %>% droplevels, 
+                                          validate %>% pull(Classification) %>% as.factor %>% droplevels, 
+                                          positive = positive.class)
+t1_df <- cm$overall %>% round(., 3) %>% t() %>%
+  cbind(.,
+        AUC = multiclass.roc(response = validate %>% pull(Classification), predictor = predict_class) %>%
+          .$auc %>% as.numeric() %>% round(3),
+        micro_F1 = matrix_one_vs_all %>% get.micro.f1() %>% round(3),
+        macro_F1 = matrix_one_vs_all %>% get.macro.f1() %>% round(3))
+if (output == "classwise"){return(matrix_test$byClass %>% reshape2::melt())}
+else {return(as.data.frame(t1_df))}
+}
 
 #####
-res <- rbind(cm3$overall,rbind(cm4$overall,cm5$overall)) %>% as.data.frame
-res$Protein <- c(rep("HA",3))
-res$Holdout <- c("H3N2","H1N2","H1N1")
-ggplot(res,aes(Holdout,y=Accuracy,ymin=AccuracyLower,ymax=AccuracyUpper,colour=Holdout))+geom_pointrange()
