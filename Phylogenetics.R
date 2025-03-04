196f60b07366b99290b7f0dbf1ed75352163f590
Jordan Bone <j.bone.1@research.gla.ac.uk>
2025-03-04 17:28:38 +0000

    Phylogenetics code

--- Phylogenetics.R
+++ Phylogenetics.R
@@ -1,19 +1,33 @@
-setwd("/Users/jordanbone/Documents/Coding_Ref/Phylogenetics/BayesTraits/")
+setwd("/Users/jordanbone/Documents/Coding_Ref/Phylogenetics/")
 source("MegaLibrary.R")
 # install_github("rgriff23/btw")
 library("btw")
 # ape::ace function could also be used to do ancestral reconstruction.
 
-HA_tree <- read.nexus("IAV/HA_tree.nexus")
+m1_root <- read.nexus("iqtree/alpha iqtree/M1.nexus")
+### For discrete characters:
+x <- ifelse(m1_root$tip.label %>% str_split_i("_",3)=="Avian",0,1) %>% as.factor
+ans <- ace(x, phy=m1_root, type="discrete", use.expm=TRUE, use.eigen=FALSE)
 
-# ace(x, HA_tree, type = "discrete", 
-#     method = if (type == "continuous")
-#   "REML" else "ML", CI = TRUE,
-#   model = if (type == "continuous") "BM" else "ER",
-#   scaled = TRUE, kappa = 1, corStruct = NULL, ip = 0.1,
-#   use.expm = FALSE, use.eigen = TRUE, marginal = FALSE)
+#### Showing the likelihoods on each node:
+plot(bird.orders, type = "c", FALSE, label.offset = 1)
+co <- c("blue", "yellow")
+tiplabels(pch = 22, bg = co[as.numeric(x)], cex = 2, adj = 1)
+nodelabels(thermo = ans$lik.anc, piecol = co, cex = 0.75)
 
 
-ace(HA_tree$tip.label, HA_tree, type = "discrete", method = "ML", CI = TRUE,
-  model = "ER", scaled = TRUE, kappa = 1, corStruct = NULL, ip = 0.1,
-  use.expm = FALSE, use.eigen = TRUE, marginal = FALSE)
+### Some random data...
+data(bird.orders)
+x <- rnorm(23)
+### Compare the three methods for continuous characters:
+ace(x, bird.orders)
+ace(x, bird.orders, method = "pic")
+ace(x, bird.orders, method = "GLS", corStruct = corBrownian(1, bird.orders))
+### For discrete characters:
+x <- factor(c(rep(0, 5), rep(1, 18)))
+ans <- ace(x, bird.orders, type = "d")
+#### Showing the likelihoods on each node:
+plot(bird.orders, type = "c", FALSE, label.offset = 1)
+co <- c("blue", "yellow")
+tiplabels(pch = 22, bg = co[as.numeric(x)], cex = 2, adj = 1)
+nodelabels(thermo = ans$lik.anc, piecol = co, cex = 0.75)
