fad1c1616e04d541c7789fa8a0ef8c83a6fb805f
Jordan Bone <j.bone.1@research.gla.ac.uk>
2025-03-06 17:09:03 +0000

    var imp figures

--- Phylogenetics.R
+++ Phylogenetics.R
@@ -2,19 +2,17 @@ setwd("/Users/jordanbone/Documents/Coding_Ref/Phylogenetics/")
 source("MegaLibrary.R")
 # install_github("rgriff23/btw")
 library("btw")
+library(tidyr)
+library(stringr)
+library(magrittr)
 # ape::ace function could also be used to do ancestral reconstruction.
 
 m1_root <- read.nexus("iqtree/alpha iqtree/M1.nexus")
+
 ### For discrete characters:
-x <- ifelse(m1_root$tip.label %>% str_split_i("_",3)=="Avian",0,1) %>% as.factor
-ans <- ace(x, phy=m1_root, type="discrete", use.expm=TRUE, use.eigen=FALSE)
-
-#### Showing the likelihoods on each node:
-plot(bird.orders, type = "c", FALSE, label.offset = 1)
-co <- c("blue", "yellow")
-tiplabels(pch = 22, bg = co[as.numeric(x)], cex = 2, adj = 1)
-nodelabels(thermo = ans$lik.anc, piecol = co, cex = 0.75)
-
+# x <- ifelse(m1_root$tip.label %>% str_split_i("_",3)=="Avian",0,1) %>% as.factor
+y <- m1_root$tip.label %>% str_split_i("_",3) %>% as.factor
+ans <- ace(y, phy=m1_root,type="discrete")
 
 ### Some random data...
 data(bird.orders)
@@ -31,3 +29,27 @@ plot(bird.orders, type = "c", FALSE, label.offset = 1)
 co <- c("blue", "yellow")
 tiplabels(pch = 22, bg = co[as.numeric(x)], cex = 2, adj = 1)
 nodelabels(thermo = ans$lik.anc, piecol = co, cex = 0.75)
+
+library(phytools)
+data(anoletree)
+## this is just to pull out the tip states from the
+## data object - normally we would read this from file
+x<-getStates(anoletree,"tips")
+tree<-anoletree
+rm(anoletree)
+tree
+plotTree(tree,type="fan",fsize=0.8,ftype="i")
+cols<-setNames(palette()[1:length(unique(x))],sort(unique(x)))
+tiplabels(pie=to.matrix(x,sort(unique(x))),piecol=cols,cex=0.3)
+add.simmap.legend(colors=cols,prompt=FALSE,x=0.9*par()$usr[1],
+                  y=-max(nodeHeights(tree)),fsize=0.8)
+fitER<-ace(x,tree,model="ER",type="discrete")
+fitER
+round(fitER$lik.anc,3)
+
+plotTree(tree,type="fan",fsize=0.8,ftype="i")
+nodelabels(node=1:tree$Nnode+Ntip(tree),
+           pie=fitER$lik.anc,piecol=cols,cex=0.5)
+tiplabels(pie=to.matrix(x,sort(unique(x))),piecol=cols,cex=0.3)
+add.simmap.legend(colors=cols,prompt=FALSE,x=0.9*par()$usr[1],
+                  y=-max(nodeHeights(tree)),fsize=0.8)
